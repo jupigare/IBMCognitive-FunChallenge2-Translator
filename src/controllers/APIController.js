d3dd67a117dbb244ada3edf40d9d80ddc21b216d
Jasmine Singh <jasmine.singh@jas.si>
2017-03-01 21:07:00 -0800

    add APIController functions to handle translation and tone analyzing

--- src/controllers/APIController.js
+++ src/controllers/APIController.js
@@ -8,10 +8,50 @@ export default {
 };
 
 function history(req, res) {
+	//To Add: retrieve history from Cloudant NoSQL DB
 	result = "History results here.";
 	res.json(result);
 }
 function translate(req, res) {
-	result = "Translate results here.";
-	res.json(result);
+	console.log("Source String:", req.query.text);
+	console.log("Dest Language:", req.query.language);
+	language_translator.translate({
+		text: req.query.text,
+		source: 'en',
+		target: req.query.language 
+		}, async function (err, translation) {
+			if (err) {
+				console.log('translate() error:', err);
+				var result = {'error':err};
+			} else {
+				var sourceTextTone = await toneAnalyze(req.query.text);
+				var translatedText = translation.translations[0].translation;
+				var translatedTextTone = await toneAnalyze(translatedText);
+				var result = {
+					'sourceTextTone':sourceTextTone,
+					'translatedText':translatedText,
+					'translatedTextTone':translatedTextTone
+				};
+				//To Add: write results data to Cloundant NoSQL DB
+			}
+			res.json(result);
+		}
+	);
+}
+
+function toneAnalyze(text,tones='social') {
+	return new Promise((resolve,reject) => {
+		tone_analyzer.tone(
+			{text:text, tones:tones},
+			function(err, tone) {
+				if (err) {
+					console.log('toneAnalyze() error:', err);
+					reject({'error':err});
+				} else {
+					resolve(tone.document_tone.tone_categories[0].tones);
+				}
+				return;
+			}
+		)
+	});		
 }
