728f303534e322c6cb1c15d35fdc079d17778990
Jasmine Singh <jasmine.singh@jas.si>
2017-03-05 18:07:20 -0800

    Minor code style changes (missing semicolons, spacing, etc.)

--- src/controllers/APIController.js
+++ src/controllers/APIController.js
@@ -2,10 +2,12 @@ import APIService from '../services/APIServiceMine';
 const credentials = APIService.credentials();
 
 const watson = require('watson-developer-cloud');
+
 const toneAnalyzer = watson.tone_analyzer(credentials.tone);
 const languageTranslator = watson.language_translator(credentials.translate);
 
 require('dotenv').load();
+
 const Cloudant = require('cloudant');
 const cloudant = Cloudant(
   {
@@ -15,7 +17,6 @@ const cloudant = Cloudant(
 );
 const db = cloudant.db.use('translatehistory');
 
-const csrf = require('csurf');
 const languages = {
   en: 'English',
   es: 'Spanish',
@@ -44,6 +45,7 @@ function history(req, res) {
       if (err) {
         console.log('Error retrieving history:', err);
         const result = err;
+        res.render('index', {data: result});
       } else {
         console.log(`Successfully retrieved history (${num} entries)`);
         var result = [];
@@ -54,12 +56,12 @@ function history(req, res) {
             translatedText: data.rows[i].doc.translatedText,
           });
         }
-        res.render('index', {data: result, csrfToken: csrf()});
+        res.render('index', {data: result});
       }
     }
   );
 }
-function translate (req, res) {
+function translate(req, res) {
   const inputs = req.query || req.params;
   console.log('Inputs:', inputs.sourceText, inputs.destinationLanguageCode);
   languageTranslator.translate(
@@ -77,12 +79,14 @@ function translate (req, res) {
         var sourceTextTone = await tone(inputs.sourceText);
         const translatedText = translation.translations[0].translation;
         const output = {
-            sourceText: inputs.sourceText,
-            destinationLanguageCode: inputs.destinationLanguageCode,
-            translatedText: translatedText,
-        }
+          sourceText: inputs.sourceText,
+          destinationLanguageCode: inputs.destinationLanguageCode,
+          translatedText: translatedText,
+        };
+        var translatedTextTone = await tone(translatedText);
+
         // Adds document if it doesn't exist yet in DB
-        db.find({selector:output}, function(err, data) {
+        db.find({ selector:output, }, function(err, data) {
             if (err||data.docs.length==0) {
               console.log('Did not locate translation; attempting to add...');
               // If doesn't exist, write translation data to Cloundant NoSQL DB
@@ -91,6 +95,7 @@ function translate (req, res) {
                   if (err) {
                     console.log('Error adding to db:', err);
                     const result = {error: err};
+                    res.render('results', {data: result});
                   } else {
                     console.log('Successfully added translation to db');
                   }
@@ -100,14 +105,13 @@ function translate (req, res) {
               console.log('Translation already exists in db; will not attempt to add');
             }
         });
-        var translatedTextTone = await tone(translatedText);
         const result = {
           sourceTextTone: sourceTextTone,
           sourceText: inputs.sourceText,
           destinationLanguage: languages[inputs.destinationLanguageCode],
           translatedText: translatedText,
           translatedTextTone: translatedTextTone,
-        }
+        };
         res.render('results', {data: result});
       }
     }
@@ -115,7 +119,7 @@ function translate (req, res) {
 }
 
 function tone (text, tones='social') {
-  return new Promise((resolve,reject) => {
+  return new Promise((resolve, reject) => {
     toneAnalyzer.tone(
       {
         text: text, 
